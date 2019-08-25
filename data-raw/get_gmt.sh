1b9e40a535a079349815ca10f91b22bd0aa1372d
Enrique Toledo M <ToledoEM@users.noreply.github.com>
2019-08-25 15:41:28 +0100

    update to version 7.0

---
+++ data-raw/get_gmt.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ ! -d "gmt" ]; then
+	mkdir gmt
+fi
+
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c1.all.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c2.cgp.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c2.cp.biocarta.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c2.cp.kegg.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c2.cp.reactome.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c2.cp.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c3.mir.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c3.tft.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c4.cgn.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c4.cm.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c5.bp.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c5.cc.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c5.mf.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c6.all.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/c7.all.v6.2.symbols.gmt
+# wget -P gmt/ ftp://ftp.broadinstitute.org/pub/gsea/gene_sets/h.all.v6.2.symbols.gmt
+
+
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/h.all.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c7.all.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c6.all.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c5.mf.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c5.cc.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c5.bp.v7.0.symbols.gmt
+#wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c5.all.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c4.cm.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c4.cgn.v7.0.symbols.gmt
+#wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c4.all.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c3.tft.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c3.mir.v7.0.symbols.gmt
+#wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c3.all.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c2.cp.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c2.cp.reactome.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c2.cp.pid.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c2.cp.kegg.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c2.cp.biocarta.v7.0.symbols.gmt
+wget -P gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c2.cgp.v7.0.symbols.gmt
+#wget -p gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c2.all.v7.0.symbols.gmt 
+wget -p gmt/ ftp://ftp.broadinstitute.org/distribution/gsea/gene_sets/c1.all.v7.0.symbols.gmt
